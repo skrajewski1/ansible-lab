e2be3f9a7adb1803fdf4a75dec667901742334f9
skrajewski1 <sean.krajewski@bonitahealthcare.com>
2024-06-25 14:46:03 -0500

    file

--- install.sh
+++ install.sh
@@ -11,3 +11,8 @@ sudo pip3.8 install ansible
 sudo pip3.8 install boto3 botocore awscli
 #change terminal color
 echo "PS1='\e[1;32m\u@\h \w$ \e[m'" >> /home/ec2-user/.bash_profile
+
+sudo yum install git -y
+#/bin/mkdir /home/ec2-user/ansible-dev
+git clone https://github.com/skrajewski1/ansible-lab2.git
+cp -r ansible-lab2/* /home/ec2-user/
